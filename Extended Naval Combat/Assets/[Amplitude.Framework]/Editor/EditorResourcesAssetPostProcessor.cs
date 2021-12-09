fa1f88b572b646bac2cdda1e89b6a445bbc5fa60
sswelm <sswelm@gmail.com>
2021-12-09 15:38:12 +0100

    initial load excluding ArtifactDB

---
+++ Extended Naval Combat/Assets/[Amplitude.Framework]/Editor/EditorResourcesAssetPostProcessor.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="EditorResourcesAssetPostProcessor.cs" company="AMPLITUDE Studios">Copyright AMPLITUDE Studios. All rights reserved.</copyright>
+
+namespace Amplitude.Framework.Editor
+{
+    internal class EditorResourcesAssetPostProcessor : UnityEditor.AssetPostprocessor
+    {
+        protected virtual void OnPreprocessTexture()
+        {
+            //// ../Assets/[Amplitude.Framework]/Editor/EditorResources/Icons/*
+
+            if (this.assetPath.Contains("Assets/[Amplitude.Framework]/Editor/EditorResources/Icons/"))
+            {
+                UnityEditor.TextureImporter textureImporter = (UnityEditor.TextureImporter)assetImporter;
+
+                textureImporter.alphaIsTransparency = true;
+                textureImporter.filterMode = UnityEngine.FilterMode.Bilinear;
+                textureImporter.mipmapEnabled = false;
+                textureImporter.maxTextureSize = 64;
+                textureImporter.npotScale = UnityEditor.TextureImporterNPOTScale.None;
+                textureImporter.textureCompression = UnityEditor.TextureImporterCompression.Compressed;
+                textureImporter.textureType = UnityEditor.TextureImporterType.GUI;
+                textureImporter.wrapMode = UnityEngine.TextureWrapMode.Clamp;
+            }
+        }
+    }
+}
